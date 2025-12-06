2959637f90735086662a449aba2655e5c9935c91
Scaramanga82 <kanazirmilos@gmail.com>
2025-12-07 00:26:59 +0100

    test bucket

--- terraform/stacks/dev/outputs.tf
+++ terraform/stacks/dev/outputs.tf
@@ -1,17 +1,14 @@
-# Ime S3 bucketa
 output "bucket_name" {
   description = "The name of the S3 bucket"
-  value       = module.my_bucket.bucket
+  value       = module.my_bucket.this_bucket_name
 }
 
-# ARN S3 bucketa
 output "bucket_arn" {
   description = "The ARN of the S3 bucket"
-  value       = module.my_bucket.arn
+  value       = module.my_bucket.this_bucket_arn
 }
 
-# ID S3 bucketa
 output "bucket_id" {
   description = "The ID of the S3 bucket"
-  value       = module.my_bucket.id
+  value       = module.my_bucket.this_bucket_id
 }
