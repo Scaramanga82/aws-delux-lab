8286146cc962a66ecc8a863fa3b48ce640e3a3ee
Scaramanga82 <kanazirmilos@gmail.com>
2025-12-07 00:41:25 +0100

    test bucket

--- terraform/env-config/dev.tfvars
+++ terraform/env-config/dev.tfvars
@@ -1,3 +1,2 @@
 aws_region         = "eu-central-1"
 env_name           = "dev"
-deploy_s3          = false
--- terraform/stacks/dev/s3.tf
+++ terraform/stacks/dev/s3.tf
@@ -1,24 +1,24 @@
-module "s3_bucket" {
-  source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "5.9.0"
+# module "s3_bucket" {
+#   source  = "terraform-aws-modules/s3-bucket/aws"
+#   version = "5.9.0"
 
-  bucket = "${var.env_name}-milos-projekat-bucket"
-  acl    = "private"
+#   bucket = "${var.env_name}-milos-projekat-bucket"
+#   acl    = "private"
 
-  versioning = {
-    enabled = false
-  }
+#   versioning = {
+#     enabled = false
+#   }
 
-  server_side_encryption_configuration = {
-    rule = {
-      apply_server_side_encryption_by_default = {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
+#   server_side_encryption_configuration = {
+#     rule = {
+#       apply_server_side_encryption_by_default = {
+#         sse_algorithm = "AES256"
+#       }
+#     }
+#   }
 
-  tags = {
-    Project     = "Milos-Projekat"
-    Environment = var.env_name
-  }
-}
+#   tags = {
+#     Project     = "Milos-Projekat"
+#     Environment = var.env_name
+#   }
+# }
