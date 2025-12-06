2dc5181fa031449adbfbd34168daf3bc4c29fb17
Scaramanga82 <kanazirmilos@gmail.com>
2025-12-06 21:33:41 +0100

    correcting module name

--- terraform/stacks/dev/s3.tf
+++ terraform/stacks/dev/s3.tf
@@ -1,6 +1,6 @@
 module "my_bucket" {
   count       = var.deploy_s3 ? 1 : 0
-  source      = "../../modules/s3_bucket"
+  source      = "../../modules/s3"
   env         = var.env
   bucket_name = "my-project-dev-bucket"
   versioning  = false
