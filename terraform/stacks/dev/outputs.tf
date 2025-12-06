c8389e70be4ba98ec9f4936c770fb9b1eaa282c0
Scaramanga82 <kanazirmilos@gmail.com>
2025-12-07 00:30:36 +0100

    test bucket

--- terraform/stacks/dev/outputs.tf
+++ terraform/stacks/dev/outputs.tf
@@ -1,14 +1,19 @@
-output "bucket_name" {
-  description = "The name of the S3 bucket"
-  value       = module.my_bucket.this_bucket_name
+output "s3_bucket_id" {
+  description = "The name of the bucket"
+  value       = module.s3_bucket.s3_bucket_id
 }
 
-output "bucket_arn" {
-  description = "The ARN of the S3 bucket"
-  value       = module.my_bucket.this_bucket_arn
+output "s3_bucket_arn" {
+  description = "The ARN of the bucket"
+  value       = module.s3_bucket.s3_bucket_arn
 }
 
-output "bucket_id" {
-  description = "The ID of the S3 bucket"
-  value       = module.my_bucket.this_bucket_id
+output "s3_bucket_region" {
+  description = "The AWS region this bucket resides in"
+  value       = module.s3_bucket.s3_bucket_region
+}
+
+output "s3_bucket_domain_name" {
+  description = "The bucket domain name"
+  value       = module.s3_bucket.s3_bucket_bucket_domain_name
 }
--- terraform/stacks/dev/s3.tf
+++ terraform/stacks/dev/s3.tf
@@ -1,4 +1,4 @@
-module "my_bucket" {
+module "s3_bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "5.9.0"
 
