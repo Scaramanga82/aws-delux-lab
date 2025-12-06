d8ca24b4092543edfab599503beb5c0c73a0e411
Scaramanga82 <kanazirmilos@gmail.com>
2025-12-07 00:20:01 +0100

    test bucket

--- terraform/stacks/dev/outputs.tf
+++ terraform/stacks/dev/outputs.tf
@@ -1,7 +1,7 @@
 output "bucket_id" {
-  value = module.my_bucket.bucket_id
+  value = module.my_bucket.this_bucket_id
 }
 
 output "bucket_arn" {
-  value = module.my_bucket.bucket_arn
+  value = module.my_bucket.this_bucket_arn
 }
