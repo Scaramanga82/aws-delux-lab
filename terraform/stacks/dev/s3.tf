11a3a80221cbb436415284e63a6c0e54b028713f
Scaramanga82 <kanazirmilos@gmail.com>
2025-12-07 00:48:21 +0100

    test s3

--- terraform/stacks/dev/s3.tf
+++ terraform/stacks/dev/s3.tf
@@ -1,24 +1,35 @@
-# module "s3_bucket" {
-#   source  = "terraform-aws-modules/s3-bucket/aws"
-#   version = "5.9.0"
+##########################################################
+# S3 Test bucket
+##########################################################
+module "s3_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "5.9.0"
 
-#   bucket = "${var.env_name}-milos-projekat-bucket"
-#   acl    = "private"
+  bucket = "${var.env_name}-milos-projekat-bucket"
+  acl    = "private"
 
-#   versioning = {
-#     enabled = false
-#   }
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
 
-#   server_side_encryption_configuration = {
-#     rule = {
-#       apply_server_side_encryption_by_default = {
-#         sse_algorithm = "AES256"
-#       }
-#     }
-#   }
+  versioning = {
+    enabled = false
+  }
 
-#   tags = {
-#     Project     = "Milos-Projekat"
-#     Environment = var.env_name
-#   }
-# }
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
+  tags = {
+    Project     = "Milos-Projekat"
+    Environment = var.env_name
+  }
+}
