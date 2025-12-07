813c7844cdada3fe908626318232135ab131c932
Scaramanga82 <kanazirmilos@gmail.com>
2025-12-07 01:20:12 +0100

    test s3

--- terraform/stacks/dev/outputs.tf
+++ terraform/stacks/dev/outputs.tf
@@ -1,22 +1,22 @@
 ##########################################################
 # S3 Test bucket Outputs
 ##########################################################
-output "s3_bucket_id" {
-  description = "The name of the bucket"
-  value       = module.s3_bucket.s3_bucket_id
-}
+# output "s3_bucket_id" {
+#   description = "The name of the bucket"
+#   value       = module.s3_bucket.s3_bucket_id
+# }
 
-output "s3_bucket_arn" {
-  description = "The ARN of the bucket"
-  value       = module.s3_bucket.s3_bucket_arn
-}
+# output "s3_bucket_arn" {
+#   description = "The ARN of the bucket"
+#   value       = module.s3_bucket.s3_bucket_arn
+# }
 
-output "s3_bucket_region" {
-  description = "The AWS region this bucket resides in"
-  value       = module.s3_bucket.s3_bucket_region
-}
+# output "s3_bucket_region" {
+#   description = "The AWS region this bucket resides in"
+#   value       = module.s3_bucket.s3_bucket_region
+# }
 
-output "s3_bucket_domain_name" {
-  description = "The bucket domain name"
-  value       = module.s3_bucket.s3_bucket_bucket_domain_name
-}
+# output "s3_bucket_domain_name" {
+#   description = "The bucket domain name"
+#   value       = module.s3_bucket.s3_bucket_bucket_domain_name
+# }
