8c51960bbe8b72773ab66166b6ffe9c5e51f90fa
Scaramanga82 <kanazirmilos@gmail.com>
2025-12-13 17:16:03 +0100

    subnet name correction

--- terraform/stacks/dev/vpc.tf
+++ terraform/stacks/dev/vpc.tf
@@ -40,12 +40,10 @@ module "vpc" {
   }
 
   public_subnet_tags = {
-    Name = "${var.project_name}-${var.env_name}-public-%s"
     Type = "public"
   }
 
   private_subnet_tags = {
-    Name = "${var.project_name}-${var.env_name}-private-%s"
     Type = "private"
   }
 
