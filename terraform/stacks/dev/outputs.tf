6224a188cc4983b7ff7df923b0bd4cc9d17a84dc
Scaramanga82 <kanazirmilos@gmail.com>
2025-12-14 22:24:52 +0100

    correcting alb sg output

--- terraform/stacks/dev/outputs.tf
+++ terraform/stacks/dev/outputs.tf
@@ -213,11 +213,6 @@ output "alb_arn" {
   value       = module.alb.arn
 }
 
-output "alb_security_group_id" {
-  description = "Security Group ID used by ALB"
-  value       = aws_security_group.alb.id
-}
-
 output "target_group_arn" {
   description = "ARN of the target group"
   value       = module.alb.target_groups["ecs"].arn
