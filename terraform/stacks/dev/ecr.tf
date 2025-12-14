1ea694be9673df5dec2d00d4aec82e48ea0cd56f
Scaramanga82 <kanazirmilos@gmail.com>
2025-12-14 23:28:33 +0100

    adding new ecr repo

--- terraform/stacks/dev/ecr.tf
+++ terraform/stacks/dev/ecr.tf
@@ -1,8 +1,57 @@
-# resource "aws_ecr_repository" "kanazir_ecr" {
-#   name                 = "kanazir-ecr"
-#   image_tag_mutability = "MUTABLE"
+##########################################################
+# ECR Repository
+##########################################################
 
-#   image_scanning_configuration {
-#     scan_on_push = true
-#   }
-# }
+resource "aws_ecr_repository" "app" {
+  name                 = "kanazir-ecr"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+
+  tags = {
+    Name        = "${var.project_name}-${var.env_name}-ecr"
+    Project     = var.project_name
+    Environment = var.env_name
+    ManagedBy   = "Terraform"
+  }
+}
+
+# Lifecycle policy - keep last 10 images
+resource "aws_ecr_lifecycle_policy" "app" {
+  repository = aws_ecr_repository.app.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep last 10 images"
+        selection = {
+          tagStatus     = "any"
+          countType     = "imageCountMoreThan"
+          countNumber   = 10
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
+
+# Output - ecr repository url
+output "ecr_repository_url" {
+  description = "URL of the ECR repository"
+  value       = aws_ecr_repository.app.repository_url
+}
+
+# Output - ecr repository arn
+output "ecr_repository_arn" {
+  description = "ARN of the ECR repository"
+  value       = aws_ecr_repository.app.arn
+}
