eeef220f6320e5e7d425af373e45874b3ece4f34
Scaramanga82 <kanazirmilos@gmail.com>
2025-12-15 03:16:42 +0100

    testing task def

--- terraform/stacks/dev/outputs.tf
+++ terraform/stacks/dev/outputs.tf
@@ -242,21 +242,21 @@ output "ecs_cluster_arn" {
   value       = module.ecs_cluster.cluster_arn
 }
 
-# ##########################################################
-# # ECS Task Definition Outputs
-# ##########################################################
+##########################################################
+# ECS Task Definition Outputs
+##########################################################
 
-# output "ecs_service_id" {
-#   description = "ID of the ECS service"
-#   value       = module.ecs_service.id
-# }
+output "ecs_service_id" {
+  description = "ID of the ECS service"
+  value       = module.ecs_service.id
+}
 
-# output "ecs_service_name" {
-#   description = "Name of the ECS service"
-#   value       = module.ecs_service.name
-# }
+output "ecs_service_name" {
+  description = "Name of the ECS service"
+  value       = module.ecs_service.name
+}
 
-# output "ecs_task_definition_arn" {
-#   description = "ARN of the task definition"
-#   value       = module.ecs_service.task_definition_arn
-# }
+output "ecs_task_definition_arn" {
+  description = "ARN of the task definition"
+  value       = module.ecs_service.task_definition_arn
+}
