c976834ca2548bd835c7bc00dde4125b8d49bdfc
Scaramanga82 <kanazirmilos@gmail.com>
2025-12-15 01:23:13 +0100

    correcting ecs cluster

--- terraform/stacks/dev/outputs.tf
+++ terraform/stacks/dev/outputs.tf
@@ -229,15 +229,15 @@ output "api_url" {
 
 output "ecs_cluster_id" {
   description = "ID of the ECS cluster"
-  value       = module.ecs_cluster.cluster.id
+  value       = module.ecs_cluster.ecs_cluster_id
 }
 
 output "ecs_cluster_name" {
   description = "Name of the ECS cluster"
-  value       = module.ecs_cluster.cluster.name
+  value       = module.ecs_cluster.ecs_cluster_name
 }
 
 output "ecs_cluster_arn" {
   description = "ARN of the ECS cluster"
-  value       = module.ecs_cluster.cluster.arn
+  value       = module.ecs_cluster.ecs_cluster_arn
 }
