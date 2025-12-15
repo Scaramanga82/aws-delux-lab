743f99356ab68793cf6934ced8bc430ccbe6ff40
Scaramanga82 <kanazirmilos@gmail.com>
2025-12-15 01:17:37 +0100

    correcting ecs cluster

--- terraform/stacks/dev/ecs-cluster.tf
+++ terraform/stacks/dev/ecs-cluster.tf
@@ -2,7 +2,7 @@
 # ECS Cluster
 ##########################################################
 
-module "ecs" {
+module "ecs_cluster" {
   source  = "terraform-aws-modules/ecs/aws"
   version = "6.10.0"
 
