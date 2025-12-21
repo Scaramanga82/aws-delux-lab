fa44a006f4d57c6a20fd54d149af19ba434d05e5
Scaramanga82 <kanazirmilos@gmail.com>
2025-12-21 15:05:57 +0100

    destroy alb

--- terraform/stacks/dev/route53.tf
+++ terraform/stacks/dev/route53.tf
@@ -1,18 +1,18 @@
-##########################################################
-# Route53 A Record
-##########################################################
+# ##########################################################
+# # Route53 A Record
+# ##########################################################
 
-resource "aws_route53_record" "api" {
-  zone_id = data.aws_route53_zone.main.zone_id
-  name    = "api.dev.kanazir.link"
-  type    = "A"
+# resource "aws_route53_record" "api" {
+#   zone_id = data.aws_route53_zone.main.zone_id
+#   name    = "api.dev.kanazir.link"
+#   type    = "A"
 
-  alias {
-    name                   = module.alb.dns_name
-    zone_id                = module.alb.zone_id
-    evaluate_target_health = true
-  }
-}
+#   alias {
+#     name                   = module.alb.dns_name
+#     zone_id                = module.alb.zone_id
+#     evaluate_target_health = true
+#   }
+# }
 
 # ##########################################################
 # # Route53 Records for Frontend (CloudFront)
