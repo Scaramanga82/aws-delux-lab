907e2b83f826f2a9e8602c72b5e90bde000dbe8f
Milos Kanazir <milos.kanazir@vegaitglobal.com>
2025-12-25 17:01:47 +0100

    testing cicd

---
+++ terraform/stacks/dev/ecs-task-def-backup.tf
@@ -0,0 +1,127 @@
+##########################################################
+# Data
+##########################################################
+
+data "aws_region" "current" {}
+
+##########################################################
+# CloudWatch Log Group
+##########################################################
+
+resource "aws_cloudwatch_log_group" "ecs_app" {
+  name              = "/aws/ecs/${var.project_name}-${var.env_name}-app/${var.project_name}"
+  retention_in_days = 7
+
+  tags = {
+    Name        = "${var.project_name}-${var.env_name}-ecs-app-logs"
+    Project     = var.project_name
+    Environment = var.env_name
+    ManagedBy   = "Terraform"
+  }
+}
+
+##########################################################
+# ECS Task Definition
+##########################################################
+
+resource "aws_ecs_task_definition" "app" {
+  family                   = "${var.project_name}-${var.env_name}-app"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+
+  cpu    = tostring(var.ecs_cpu)
+  memory = tostring(var.ecs_memory)
+
+  execution_role_arn = aws_iam_role.ecs_task_execution.arn
+  task_role_arn      = aws_iam_role.ecs_task_app.arn
+
+  depends_on = [
+    aws_cloudwatch_log_group.ecs_app
+  ]
+
+  container_definitions = jsonencode([
+    {
+      name      = var.project_name
+      image     = var.ecs_image
+      essential = true
+
+      portMappings = [
+        {
+          containerPort = var.ecs_container_port
+          protocol      = "tcp"
+        }
+      ]
+
+      environment = [
+        {
+          name  = "ENVIRONMENT"
+          value = var.env_name
+        },
+        {
+          name  = "PORT"
+          value = tostring(var.ecs_container_port)
+        }
+      ]
+
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = aws_cloudwatch_log_group.ecs_app.name
+          awslogs-region        = data.aws_region.current.id
+          awslogs-stream-prefix = "ecs"
+        }
+      }
+
+      readonlyRootFilesystem = false
+    }
+  ])
+
+  tags = {
+    Name        = "${var.project_name}-${var.env_name}-ecs-task"
+    Project     = var.project_name
+    Environment = var.env_name
+    ManagedBy   = "Terraform"
+  }
+}
+
+##########################################################
+# ECS Service
+##########################################################
+
+resource "aws_ecs_service" "app" {
+  name            = "${var.project_name}-${var.env_name}-app"
+  cluster         = module.ecs_cluster.cluster_arn
+  task_definition = aws_ecs_task_definition.app.arn
+  desired_count   = var.ecs_desired_count
+  launch_type     = "FARGATE"
+
+  enable_execute_command = true
+
+  network_configuration {
+    subnets         = module.vpc.private_subnets
+    security_groups = [aws_security_group.ecs_tasks.id]
+    assign_public_ip = false
+  }
+
+  load_balancer {
+    target_group_arn = module.alb.target_groups["ecs"].arn
+    container_name   = var.project_name
+    container_port   = var.ecs_container_port
+  }
+
+  health_check_grace_period_seconds = 60
+
+  deployment_minimum_healthy_percent = 50
+  deployment_maximum_percent         = 200
+
+  force_new_deployment  = true
+  wait_for_steady_state = false
+
+  enable_ecs_managed_tags = true
+  tags = {
+    Name        = "${var.project_name}-${var.env_name}-ecs-service"
+    Project     = var.project_name
+    Environment = var.env_name
+    ManagedBy   = "Terraform"
+  }
+}
